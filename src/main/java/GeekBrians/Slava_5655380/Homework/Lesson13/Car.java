1c38e383fae11debd1926950eae85236a0d482a4
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-02-21 16:03:28 +0300

    Решение заданий урока 13

--- src/main/java/GeekBrians/Slava_5655380/Homework/Lesson13/Car.java
+++ src/main/java/GeekBrians/Slava_5655380/Homework/Lesson13/Car.java
@@ -23,11 +23,14 @@ public class Car implements Runnable {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
             System.out.println(this.name + " готов");
+            // Все участники должны стартовать одновременно, несмотря на разное время  подготовки.
+            race.getStartBarrier().await();
         } catch (Exception e) {
             e.printStackTrace();
         }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        race.crossFinish(this);
     }
 }
--- src/main/java/GeekBrians/Slava_5655380/Homework/Lesson13/MainClass.java
+++ src/main/java/GeekBrians/Slava_5655380/Homework/Lesson13/MainClass.java
@@ -2,9 +2,9 @@ package GeekBrians.Slava_5655380.Homework.Lesson13;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Race race = new Race(CARS_COUNT, new Road(60), new Tunnel(CARS_COUNT / 2), new Road(40));
         Car[] cars = new Car[CARS_COUNT];
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
@@ -12,7 +12,11 @@ public class MainClass {
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
+        race.waitUntilStart();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        // Когда все завершат гонку, нужно выдать объявление об окончании.
+        race.waitUntilFinish();
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        System.out.println("ПОБЕДИТЕЛЬ: " + race.getWinner());
     }
 }
